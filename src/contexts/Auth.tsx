e7671bfb8aee383e70ee8b5c9be332708774e6ac
Kaue Pereira <kauefontes@outlook.com>
2022-10-31 22:11:12 -0400

    handling auth data

--- src/contexts/Auth.tsx
+++ src/contexts/Auth.tsx
@@ -1,5 +1,7 @@
-import { createContext, useState } from "react";
-import { authService } from "../services/authService";
+import { createContext, useContext, useEffect, useState } from 'react'
+import { Alert } from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { authService } from '../services/authService'
 
 export interface AuthData {
     token: string
@@ -10,6 +12,7 @@ interface AuthContextData {
     authData?: AuthData   
     signIn: (email: string, password: string) => Promise<AuthData>
     signOut: () => Promise<void>
+    loading: boolean
 }
 
 export const AuthContext = createContext<AuthContextData>(
@@ -18,19 +21,44 @@ export const AuthContext = createContext<AuthContextData>(
 
 export const AuthProvider: React.FC  = ({children}) => {
     const [authData, setAuthData] = useState<AuthData>()
-    
-    async function signIn(email: string, password: string): Promise<AuthData>{
-        setAuthData(authData)
-        return await authService.signIn(email, password)
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        loadFromStorage()
+    }, [])
+
+    async function loadFromStorage() {
+        const auth = await AsyncStorage.getItem('@AuthData')
+        if(auth) {
+            setAuthData(JSON.parse(auth))
+        }
+        setLoading(false)
     }
+    
+    async function signIn(email: string, password: string): Promise<AuthData> {
+        try {
+            const auth = await authService.signIn(email, password)
+            setAuthData(auth)
+            AsyncStorage.setItem('@AuthData', JSON.stringify(auth))
+            return auth
+        } catch (error) {
+            Alert.alert(error.message, 'Try again')
+        }
+    }
+    
     async function signOut(): Promise<void> {
         setAuthData(undefined)
+        AsyncStorage.removeItem('@AuthData')
         return
     }
 
     return (
-        <AuthContext.Provider value={{authData, signIn, signOut}}>
+        <AuthContext.Provider value={{authData, loading, signIn, signOut}}>
             {children}
         </AuthContext.Provider>
     )
+}
+
+export function useAuth() {
+    return useContext(AuthContext)
 }
