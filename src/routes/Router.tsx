c98d2c0f05c32402a56183f55a4c0a7e8180abaa
Kaue Pereira <kauefontes@outlook.com>
2022-10-31 22:12:25 -0400

    handling auth data

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { AppStack } from './AppStack'
 import { AuthStack } from './AuthStack'
+import { useAuth } from '../contexts/Auth'
+import { Text, View } from 'react-native'
 
 export function Router() {
+    const { authData, loading } = useAuth() 
 
-    const authenticated = false
+    if(loading) {
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <Text>Loading data...</Text>
+        </View>
+    }
     return (    
         <NavigationContainer>
-            { authenticated ? <AppStack /> : <AuthStack /> }
+            { authData ? <AppStack /> : <AuthStack /> }
         </NavigationContainer>    
     )
 }
