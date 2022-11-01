5a545c54b28295b69b1758a44f52bf8c22af6ed0
Kaue Pereira <kauefontes@outlook.com>
2022-10-31 22:12:59 -0400

    using context to do logout

--- src/screens/Settings.tsx
+++ src/screens/Settings.tsx
@@ -1,17 +1,17 @@
 import { Text, View } from "react-native";
 import { Button } from "../components/Button";
 import { styles } from "./styles";
-import { useNavigation } from '@react-navigation/native'
+import { useAuth } from "../contexts/Auth";
 
 export function Settings(){
-    const navigation = useNavigation()
+    const { signOut } = useAuth()
     return (
         <View style={styles.container}>
             <Text style={styles.title}>
                 Settings
             </Text>
             <Button 
-                onPress={() => console.log('logout')}
+                onPress={signOut}
                 style={{backgroundColor: 'red'}}
                 title='Log out'
             />
